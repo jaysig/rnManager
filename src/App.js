e242cab09d4efec0608ad74befdfb6ad9e571bee
Jaysig <jasontsigmon@gmail.com>
2017-03-08 16:25:00 -0600

    Starting the LoginForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
-import reducers from './src/reducers';
+import reducers from './reducers';
+import config from '../config/default.json';
+import LoginForm from './components/LoginForm';
 // import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 
 class App extends Component {
 
+  componentWillMount() {
+    // const fbConfig = config.get('private.firebase');
+    firebase.initializeApp(config.private.firebase);
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.setState({ loggedIn: true });
+    //   } else {
+    //     this.setState({ loggedIn: false });
+    //   }
+    // });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>This is a feature</Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card } from './common';
+import { View, Text } from 'react-native';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+
+class LoginForm extends Component {
+
+  render() {
+    return (
+      <Card>
+        {/* <Text>This is a feature</Text> */}
+      </Card>
+    );
+  }
+}
+// function mapStateToProps(state) {
+//  return { authenticated: state.auth.authenticated };
+// }
+// export default connect(mapStateToProps, actions)(App)
+export default LoginForm;
