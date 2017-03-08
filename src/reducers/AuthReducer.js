c10aab095be133251dd54b8174c60bff1da3ffd8
Jaysig <jasontsigmon@gmail.com>
2017-03-08 16:55:03 -0600

    Added EMAIL_CHANGED type

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,11 @@
-const INITIAL_STATE = { email: ''};
+import { EMAIL_CHANGED } from '../actions/types';
+
+const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    // case 'YOUR_ACT':
-    //   return action.payload;
+    case EMAIL_CHANGED:
+      return action.payload;
     default:
       return state;
   }
