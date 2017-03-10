73ca76cf3828775584986609ba233d04c8a884d2
Jaysig <jasontsigmon@gmail.com>
2017-03-10 12:00:31 -0600

    Added a date picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
-// import { connect } from 'react-redux';
-// import * as actions from '../../actions';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
 
@@ -12,6 +13,8 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Jane"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ props: 'name', value })}
           />
         </CardSection>
 
@@ -19,14 +22,25 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ props: 'phone', value })}
           />
         </CardSection>
 
         <CardSection>
-          <Input
-            label="Name"
-            placeholder="Jane"
-          />
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({ props: 'shift', value })}
+            style={{ flex: 1 }}
+          >
+             <Picker.Item label="Monday" value="Monday" />
+             <Picker.Item label="Tuesday" value="Tuesday" />
+             <Picker.Item label="Wednesday" value="Wednesday" />
+             <Picker.Item label="Thursday" value="Thursday" />
+             <Picker.Item label="Friday" value="Friday" />
+             <Picker.Item label="Saturday" value="Saturday" />
+             <Picker.Item label="Sunday" value="Sunday" />
+        </Picker>
         </CardSection>
 
         <CardSection>
@@ -38,13 +52,13 @@ class EmployeeCreate extends Component {
     );
   }
 }
-// const mapStateToProps = state => {
-//  return { e: state.auth.e };
-// }
+const mapStateToProps = ({ employeeForm }) => {
+  const { name, phone, shift } = employeeForm;
+ return { name, phone, shift };
+};
 // const styles = {
 //  view:{
 //
 //  }
 //}
-// export default connect(mapStateToProps, actions)(App)
-export default EmployeeCreate;
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
