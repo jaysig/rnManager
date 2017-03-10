211388b8974b5ef6770625a0e67ec5a1d3fe50e5
Jaysig <jasontsigmon@gmail.com>
2017-03-10 11:18:06 -0600

    created employee list

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+
+class EmployeeList extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>This is a feature</Text>
+      </View>
+    );
+  }
+}
+// const mapStateToProps = state => {
+//  return { e: state.auth.e };
+// }
+// export default connect(mapStateToProps, actions)(App)
+export default EmployeeList;
