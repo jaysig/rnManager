d33833d8732522342e6cb2393b195315a89225db
Jaysig <jasontsigmon@gmail.com>
2017-03-10 11:26:53 -0600

    created auth and main scene buckets

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -23,6 +25,9 @@ export const passwordChanged = (text) => {
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+
+  // Navigate to the employeeList Page
+  Actions.main();
 };
 
 const loginUserFail = (dispatch, user) => {
@@ -32,7 +37,7 @@ const loginUserFail = (dispatch, user) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
-    
+
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(user => loginUserSuccess(dispatch, user))
     .catch(() => {
