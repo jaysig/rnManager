c4b4ae83d6166355d41b96df8e7b3e893d75302d
Jaysig <jasontsigmon@gmail.com>
2017-03-11 18:07:10 -0600

    Able to pass employee data to the employee create form

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 // import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 
 class ListItem extends Component {
 
+  onRowPress() {
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          { name }
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              { name }
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -32,5 +41,5 @@ const styles = {
     fontSize: 18,
     paddingLeft: 15
   }
-}
+};
 export default ListItem;
