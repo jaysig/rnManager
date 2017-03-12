ffbe12542f75ddae0289bb595b158b8d3fd39bb9
Jaysig <jasontsigmon@gmail.com>
2017-03-12 15:15:52 -0500

    Trying to get confirm to work

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -5,10 +5,12 @@ import Communications from 'react-native-communications';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 // import * as actions from '../../actions';
 
 class EmployeeEdit extends Component {
+
+  state = { showModal: false };
 
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
@@ -43,6 +45,18 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
